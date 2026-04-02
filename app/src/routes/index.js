0607db5852a157c1e58a65d65f860abf862fa366
Anup Bhattarai <your_email@example.com>
2026-04-01 22:00:04 -0600

    checkpoint 3

--- app/src/routes/index.js
+++ app/src/routes/index.js
@@ -2,6 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hello World' });
+  res.render('index', { title: 'Grocery Flow' });
 });
+
+router.get('/lists/new', function(req, res, next) {
+  res.render('create', { title: 'Create List - Grocery Flow' });
+});
+
 module.exports = router;
